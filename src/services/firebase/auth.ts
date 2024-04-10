b61218fa61fa442aa7b5d0e56f3e2222480ba779
herald-noel <bontilao.heraldnoel@gmail.com>
2024-04-10 19:49:17 +0800

    Add firebase user auth

---
+++ src/services/firebase/auth.ts
@@ -0,0 +1,32 @@
+import { createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, updatePassword } from 'firebase/auth'
+import { auth } from './firebase'
+
+export const doCreateUserWithEmailAndPassword = async (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const doSignOut = () => {
+  return auth.signOut();
+}
+
+/*
+export const doPasswordReset = (email: string) => {
+  return sendPasswordResetEmail(auth, email)
+}
+
+export const doPasswordChange = (password: string) => {
+  if (auth.currentUser) {
+  return updatePassword(auth.currentUser, password)
+  }
+  return null
+}
+
+export const doSendEmailVerification = () => {
+  if (auth.currentUser) {
+  return sendEmailVerification(auth.currentUser, {
+    url: `${window.location.origin}/home`
+  })
+  }
+  return null
+}
+*/
