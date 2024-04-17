05a891b75536d37e9cdc761168c1da4567e22824
harknoel <bontilao.harknoel@gmail.com>
2024-04-17 18:50:15 +0800

    Fix the drawer not opening for sign in and sign up

--- src/pages/LandingPage/components/Navbar.jsx
+++ src/pages/LandingPage/components/Navbar.jsx
@@ -1,23 +1,25 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { pages } from '../../../data/pages.constants';
-import { navItems } from '../../../data/navItems.constants';
-import SignInButton from '../../SignIn/SignInButton';
-import SignUpButton from '../../SignUp/SignUpButton';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { pages } from "../../../data/pages.constants";
+import SignInButton from "../../SignIn/SignInButton";
+import SignUpButton from "../../SignUp/SignUpButton";
+import { openSignIn } from "../../SignIn/signInFormDialogSlice";
+import { openSignUp } from "../../SignUp/signUpFromDialogSlice";
+import { useDispatch } from "react-redux";
 
 const drawerWidth = 240;
 
@@ -25,31 +27,42 @@ function NavBar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
+  const dispatch = useDispatch();
+
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant='h6' sx={{ my: 2 }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
         UseClass
       </Typography>
       <Divider />
       <List>
         {pages.map((page) => (
           <ListItem key={page} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: "center" }}>
               <ListItemText primary={page} />
             </ListItemButton>
           </ListItem>
         ))}
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText
+              primary={"Log In"}
+              onClick={() => dispatch(openSignIn())}
+            />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText
+              primary={"Sign up"}
+              onClick={() => dispatch(openSignUp())}
+            />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
@@ -61,39 +74,39 @@ function NavBar(props) {
     <Box>
       <CssBaseline />
       <AppBar
-        color='default'
-        component='nav'
+        color="default"
+        component="nav"
         elevation={0}
-        style={{ background: 'transparent' }}
+        style={{ background: "transparent" }}
       >
         <Toolbar>
           <IconButton
-            color='inherit'
-            aria-label='open drawer'
-            edge='start'
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Box
             sx={{
               display: {
-                xs: 'none',
-                md: 'flex',
+                xs: "none",
+                md: "flex",
                 flexGrow: 1,
               },
             }}
           >
-            <img src='brand_icon.svg' />
+            <img src="brand_icon.svg" />
             <Button>Use Class</Button>
             {pages.map((page) => (
-              <Button key={page} sx={{ color: 'black', display: 'block' }}>
+              <Button key={page} sx={{ color: "black", display: "block" }}>
                 {page}
               </Button>
             ))}
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'block', flexGrowGrow: 0 } }}>
+          <Box sx={{ display: { xs: "none", sm: "block", flexGrowGrow: 0 } }}>
             <SignInButton />
             <SignUpButton />
           </Box>
@@ -102,16 +115,16 @@ function NavBar(props) {
       <nav>
         <Drawer
           container={container}
-          variant='temporary'
+          variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
               width: drawerWidth,
             },
           }}
