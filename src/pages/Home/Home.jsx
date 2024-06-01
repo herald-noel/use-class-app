a1f74e3b550cdfb1a492db87b8a29732cf4efa00
herald-noel <83816898+herald-noel@users.noreply.github.com>
2024-06-01 23:50:53 +0800

    Remove unused code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,12 @@
-import { Box, CssBaseline, Stack } from '@mui/material';
-
-import Form from './components/Form';
-import ConvertButton from './components/ConvertButton';
+import { Box, CssBaseline } from '@mui/material';
 import { observer } from 'mobx-react';
 import HomeViewModel from '../../viewModels/HomeViewModel';
 import { DrawerHeader } from './styles/layoutStyles';
-import PreviewButton from './components/PreviewButton';
-import ClassDiagram from './components/Mermaid/ClassDiagram';
 import SideNav from './components/SideNav';
 import TopNav from './components/TopNav';
 import MainContent from './components/MainContent';
 
 const Home = observer(() => {
-  const mermaidSource = `
-classDiagram
-  Animal <|-- Duck
-  Animal <|-- Fish
-  Animal <|-- Zebra
-  Animal : +int age
-  Animal : +String gender
-  Animal: +isMammal()
-  Animal: +mate()
-  class Duck {
-    +String beakColor
-    +swim()
-    +quack()
-  }
-  class Fish {
-    -int sizeInFeet
-    -canEat()
-  }
-  class Zebra {
-    +bool isCool
-    +run()
-  }
-`;
   const open = HomeViewModel.isSideNavOpen;
 
   const handleDrawerOpen = () => {
