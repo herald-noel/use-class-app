0b9deb95dfeb580c05153db43b5cb5765adcc0be
herald-noel <83816898+herald-noel@users.noreply.github.com>
2024-06-02 23:47:55 +0800

    Refactor and create layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,13 @@
-import { Box, CssBaseline } from '@mui/material';
 import { observer } from 'mobx-react';
-import HomeViewModel from '../../viewModels/HomeViewModel';
-import { DrawerHeader } from './styles/layoutStyles';
-import SideNav from './components/SideNav';
-import TopNav from './components/TopNav';
 import MainContent from './components/MainContent';
+import AuthLayout from '../../layouts/AuthLayout';
 
 const Home = observer(() => {
-  const open = HomeViewModel.isSideNavOpen;
-
-  const handleDrawerClick = () => {
-    HomeViewModel.toggleSignInModal();
-  };
-
   return (
     <>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <TopNav open={open} handleDrawerOpen={handleDrawerClick} />
-        <SideNav open={open} handleDrawerClose={handleDrawerClick} />
-        <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
-          <MainContent />
-        </Box>
-      </Box>
+      <AuthLayout>
+        <MainContent />
+      </AuthLayout>
     </>
   );
 });
