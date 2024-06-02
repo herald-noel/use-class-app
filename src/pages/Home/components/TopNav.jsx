2acd45859609dcb4b5df4da35cb9bd55b87d327a
harknoel <bontilao.harknoel@gmail.com>
2024-06-02 23:18:51 +0800

    Add logout

--- src/pages/Home/components/TopNav.jsx
+++ src/pages/Home/components/TopNav.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { AppBar } from "../styles/layoutStyles";
 import { BRAND_NAME } from "../../../data/config.constants";
-import { IconButton, Toolbar, Typography, Box } from "@mui/material";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import {
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import useLogout from "../hooks/useLogout";
+import UserActionMenu from "./UserMenu/UserActionMenu";
 
 const TopNav = (props) => {
+  const { logoutUser } = useLogout();
   const { open, handleDrawerOpen } = props;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    handlePopoverClose();
+    logoutUser();
+  };
+
   return (
     <AppBar position="fixed" open={open}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
@@ -33,12 +56,7 @@ const TopNav = (props) => {
             {BRAND_NAME}
           </Typography>
         </Box>
-
-        <Box>
-          <IconButton color="inherit">
-            <AccountCircleRoundedIcon />
-          </IconButton>
-        </Box>
+        <UserActionMenu anchorEl={anchorEl} handlePopoverOpen={handlePopoverOpen} handlePopoverClose={handlePopoverClose} handleLogout={handleLogout}/>
       </Toolbar>
     </AppBar>
   );
