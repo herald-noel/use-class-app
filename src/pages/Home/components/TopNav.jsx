390cafa95ec616d750b1d8d4629686868ece3c53
harknoel <bontilao.harknoel@gmail.com>
2024-06-02 14:06:27 +0800

    Add profile icon

--- src/pages/Home/components/TopNav.jsx
+++ src/pages/Home/components/TopNav.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import { AppBar } from '../styles/layoutStyles';
-import { BRAND_NAME } from '../../../data/config.constants';
-import { IconButton, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import { AppBar } from "../styles/layoutStyles";
+import { BRAND_NAME } from "../../../data/config.constants";
+import { IconButton, Toolbar, Typography, Box } from "@mui/material";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const TopNav = (props) => {
   const { open, handleDrawerOpen } = props;
   return (
-    <AppBar position='fixed' open={open}>
-      <Toolbar>
-        <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          onClick={handleDrawerOpen}
-          edge='start'
+    <AppBar position="fixed" open={open}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box
           sx={{
-            marginRight: 5,
-            ...(open && { display: 'none' }),
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant='h6' noWrap component='div'>
-          {BRAND_NAME}
-        </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {BRAND_NAME}
+          </Typography>
+        </Box>
+
+        <Box>
+          <IconButton color="inherit">
+            <AccountCircleRoundedIcon />
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
