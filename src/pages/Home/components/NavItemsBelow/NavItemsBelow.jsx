b8eca66f59e1aa0dedbe657a9283231a18d5dfe4
harknoel <bontilao.harknoel@gmail.com>
2024-06-03 01:09:03 +0800

    Add icons

--- src/pages/Home/components/NavItemsBelow/NavItemsBelow.jsx
+++ src/pages/Home/components/NavItemsBelow/NavItemsBelow.jsx
@@ -1,13 +1,26 @@
-import { List } from '@mui/material';
-import NavItem from './components/NavItem';
-import LogoutIcon from '@mui/icons-material/Logout';
-import useLogout from '../../hooks/useLogout';
+import { List, Box } from "@mui/material";
+import NavItem from "./components/NavItem";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LayersIcon from "@mui/icons-material/Layers";
+import useLogout from "../../hooks/useLogout";
+import HomeIcon from "@mui/icons-material/Home";
 
 const NavItemsBelow = () => {
   const { logoutUser } = useLogout();
   return (
-    <List>
-      <NavItem Icon={LogoutIcon} listName='Log out' handleClick={logoutUser} />
+    <List
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
+      <Box>
+        <NavItem Icon={HomeIcon} listName="Home" />
+        <NavItem Icon={LayersIcon} listName="Saved" />
+      </Box>
+      <NavItem Icon={LogoutIcon} listName="Log out" handleClick={logoutUser} />
     </List>
   );
 };
