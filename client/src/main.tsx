758286cc3399a7146b8900548102b8126b10b153
herald-noel <bontilao.heraldnoel@gmail.com>
2024-09-24 17:17:58 +0800

    Disable dev tools when in prod

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Toaster } from '@/components/ui/toaster'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { ENV } from './data/env.constants.ts'
+
+if (import.meta.env.VITE_ENV === ENV.PROD) disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
