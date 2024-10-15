98872092acb7128e391d5589f1725af9297a458d
harknoel <bontilao.harknoel@gmail.com>
2024-10-15 19:39:53 +0800

    feat(server): change api keys

--- server/src/services/ChatService.ts
+++ server/src/services/ChatService.ts
@@ -17,34 +17,44 @@ export class ChatService {
 
   async convert(plantUML: string) {
     let retries = 0;
+
     while (retries < this.maxRetries) {
       try {
+
         const userPrompt = new Prompt(
           plantUML,
           CLASS_INSTRUCTION,
           CLASS_JSON_FORMAT
         );
+
         const response = await chatUtil(userPrompt.prompt, this.currentApi);
+
+        // JSON PRE-PROCESSING
         const json = response.choices[0]?.message?.content;
         const jsonString = JSON.stringify(json);
         const cleanJson = jsonrepair(jsonString);
+
         console.log(JSON.parse(cleanJson));
         return JSON.parse(cleanJson);
       } catch (error) {
+
         console.error(
           `Error in ChatService convert method (attempt ${retries + 1}):`,
           error
         );
+
         if (isTooManyRequests(error)) {
+          retries++;
           this.currentApi = switchApiKey(this.currentApi);
+          console.log("error test");
           continue;
         }
+
         if (isRetryableError(error) && retries < this.maxRetries - 1) {
           retries++;
           await delay(this.retryDelay * retries);
           continue;
         }
-        throw error;
       }
     }
     throw new Error("Max retries reached");
